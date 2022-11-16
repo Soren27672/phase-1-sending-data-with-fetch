be87c8434c8730d109a116cff396f05500f95da6
Maxwell Gottesman <maxgman16@yahoo.com>
2022-11-16 12:32:39 -0500

    commit tree 86c6b173a8c74c1d8b5d817eabb9a2142091e368

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Add your code here
+const init = () => {
+
+    const form = document.querySelector('form');
+    const nameInput = document.querySelector('#nameInput');
+    const breedInput = document.querySelector('#breedInput');
+
+    form.addEventListener('submit',e => {
+        e.preventDefault();
+
+        fetch("http://localhost:3000/dogs",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                dogName: nameInput.value,
+                dogBreed: breedInput.value
+            })
+        })
+    })
+}
+
+document.addEventListener('DOMContentLoaded',init);
+
+function submitData(name,email) {
+        
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        })
+    })
+    .then(res => res.json())
+    .then(json => {
+        const newNode = document.createElement('p');
+        newNode.textContent = json.id;
+        newNode.classList.add("addedId");
+        document.querySelector('body').appendChild(newNode);
+    })
+    .catch(obj => {
+        const newNode = document.createElement('p');
+        newNode.textContent = obj.message;
+        newNode.classList.add("error");
+        document.querySelector('body').appendChild(newNode)
+    });
+
+    
+
+}
